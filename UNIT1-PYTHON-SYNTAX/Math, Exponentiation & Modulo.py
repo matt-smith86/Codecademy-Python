9787e72dd63a05556dd87c7b6f02949e9e459833
matt-smith86 <matt_smith86@hotmail.com>
2015-10-29 22:09:32 -0400

    Create Math, Exponentiation & Modulo.py

---
+++ UNIT1-PYTHON-SYNTAX/Math, Exponentiation & Modulo.py
@@ -0,0 +1,21 @@
+# Set count_to equal to the sum of two big numbers
+count_to = 72 + 23
+
+
+print count_to
+
+
+#Create a new variable called eggs and use exponents to set eggs equal 100.
+#Try raising 10 to the power of 2.
+
+eggs = 10**2
+
+print eggs
+
+
+#Use modulo to set spam equal to 1. You can use any two numbers that will leave a remainder of 1 to do this.
+
+
+spam = 3 % 2
+
+print spam
