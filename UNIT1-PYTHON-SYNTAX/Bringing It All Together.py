66f95f2ada7632d229a07384ee4643f13c387fcb
matt-smith86 <matt_smith86@hotmail.com>
2015-10-29 22:11:08 -0400

    Create Bringing It All Together.py

---
+++ UNIT1-PYTHON-SYNTAX/Bringing It All Together.py
@@ -0,0 +1,9 @@
+#Write a single-line comment on line 1. It can be anything! (Make sure it starts with #)
+#Set the variable monty equal to True.
+#Set another variable python equal to 1.234.
+#Set a third variable monty_python equal to python squared.
+
+# Single line comment
+monty = True
+python = 1.234
+monty_python = python **2
