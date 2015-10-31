5a911621716f264503ad252cabd7b0a1171d481d
matt-smith86 <matt_smith86@hotmail.com>
2015-10-31 19:38:33 -0400

    Create 4)Num Squared.py

---
+++ 4)Num Squared.py
@@ -0,0 +1,9 @@
+# Create a function that returns the square of a number.
+
+def square(n):
+
+    squared = n**2
+    print "%d squared is %d." % (n, squared)
+    return squared
+    
+print square(10)
